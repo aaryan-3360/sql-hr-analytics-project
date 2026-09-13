6a374b03988a453d25178a6e661f5c18a857d128
Aaryan <aaryankamboj520@gmail.com>
2026-09-13 10:00:44 +0530

    Update 1_rank_by_salary.sql

--- queries/1_rank_by_salary.sql
+++ queries/1_rank_by_salary.sql
@@ -1,1 +1,11 @@
 
+-- rank by desired salary per department
+select e.empid,e.FirstName,e.LastName,
+r.`Desired Salary`,
+e.DepartmentType,
+row_number() over(partition by e.DepartmentType order by r.`Desired Salary` desc) as Salary_rank
+from employee as e
+join recruitment as r
+on e.empid = r.`Applicant ID`;
+
+
