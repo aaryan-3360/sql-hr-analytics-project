58083f664c52cbc7c47a678cb69dd0f3f95c11c1
Aaryan <aaryankamboj520@gmail.com>
2026-09-13 10:04:11 +0530

    Add SQL query for average salary comparison by department
    This query calculates the average salary per department and compares each employee's desired salary to their department's average salary.

--- queries/2_avg_salary_comparison.sql
+++ queries/2_avg_salary_comparison.sql
@@ -1,1 +1,13 @@
-
+-- Average Salary per department and each employee compare to their department average
+select 
+e.empid,
+e.FirstName,
+e.LastName,
+e.DepartmentType,
+round(avg(r.`Desired Salary`) over(partition by e.DepartmentType),2) as avg_department_salary,
+round(r.`Desired Salary` - avg(r.`Desired Salary`) over(partition by e.DepartmentType),2) as  diff_from_avg
+from employee as e
+join recruitment as r
+on e.empid= r.`Applicant ID`
+order by e.DepartmentType,r.`Desired Salary` Desc;
+;
