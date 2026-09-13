963c6515e1c750cc2cf13eabd44fefd30fc48b88
Aaryan <aaryankamboj520@gmail.com>
2026-09-13 10:04:50 +0530

    Update 3_top_10_percent.sql

--- queries/3_top_10_percent.sql
+++ queries/3_top_10_percent.sql
@@ -1,1 +1,12 @@
 
+-- Top 10% earners company wide 
+
+select * 
+from
+(select e.empid,r.`Applicant ID`,e.FirstName,e.LastName,r.`Desired Salary`,
+ntile(10) over(order by r.`Desired Salary` desc) as top_earners
+from employee as e
+join recruitment as r
+on e.empid = r.`Applicant ID`
+) as t
+where top_earners = 1;
