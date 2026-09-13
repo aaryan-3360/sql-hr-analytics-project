fa81c1da998645b7d4759451a703a3e7487bcc95
Aaryan <aaryankamboj520@gmail.com>
2026-09-13 10:05:56 +0530

    Add SQL query for employees with long tenure
    Query to select employees with 5 or more years in the same role.

--- queries/5_long_tenure.sql
+++ queries/5_long_tenure.sql
@@ -1,1 +1,14 @@
+-- Employees 5+ Years with same role
+
+select * 
+from 
+(
+select e.empid,e.FirstName,e.LastName,e.DepartmentType,e.JobFunctionDescription,e.StartDate,
+TIMESTAMPDIFF(YEAR, STR_TO_DATE(e.StartDate,'%d-%b-%y'), CURDATE()) as Years_In_Same_Role
+from employee as e
+where ExitDate = ""
+order by  Years_In_Same_Role desc
+)as t 
+where Years_In_Same_Role >=5;
+
 
