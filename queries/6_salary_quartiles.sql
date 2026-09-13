baeb4d528faa8309485de796ab1e0eb26eb18589
Aaryan <aaryankamboj520@gmail.com>
2026-09-13 10:06:35 +0530

    Add salary quartiles classification to query

--- queries/6_salary_quartiles.sql
+++ queries/6_salary_quartiles.sql
@@ -1,1 +1,17 @@
 
+select *,
+case 
+when rnk=1 then 'Top 25%'
+when rnk=2 then 'upper mid'
+when rnk=3 then 'lower mid'
+else  'bottom 25%'
+end as Salary_Band
+from
+(
+select e.empid,e.FirstName,e.LastName,e.DepartmentType,r.`Desired Salary`,
+ntile(4) over( partition by DepartmentType order by r.`Desired Salary` desc) as rnk
+from  employee as e
+inner join recruitment as r
+on e.empid = r.`Applicant ID`
+) as t
+order by DepartmentType, rnk asc;
